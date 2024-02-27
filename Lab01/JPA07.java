3a5f47aeab463e08179ecc9912ac65762d0e3faf
handasdBoy <161306517+handasdBoy@users.noreply.github.com>
2024-02-27 16:19:16 +0800

    Add files via upload

---
+++ Lab01/JPA07.java
@@ -0,0 +1,26 @@
+public class JPA07{
+    public static void main(String[] args){
+        int action = 1;
+        int skill = 2;
+        int team = 3;
+        System.out.println("The basketball grade is " + Basketball.calGrade(action,skill,team));
+        System.out.println("The basketball grade is " + Baseball.calGrade(skill,team));
+    }
+}
+
+class Basketball{
+    public static int calGrade(int a,int s,int t)  {
+        return a+s+t;
+    }
+}
+
+class Baseball{
+    public static int calGrade(int s,int t)  {
+        return 10+s+t;
+    }
+}
+
+
+
+
+
