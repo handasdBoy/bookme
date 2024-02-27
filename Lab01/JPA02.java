166ed3d2f2cea2260e30ef234ae2ccfe06be3484
handasdBoy <161306517+handasdBoy@users.noreply.github.com>
2024-02-27 11:12:33 +0800

    Update JPA02.java

--- Lab01/JPA02.java
+++ Lab01/JPA02.java
@@ -1,4 +1,4 @@
-/* 
+
 import java.util.Scanner;
 public class aaa{
     public static void main(String[] args){
@@ -7,19 +7,5 @@ public class aaa{
         double b = a.nextDouble();
         double c = (b*2.20462);
         System.out.println(b+" kg = "+c+" ponds");
-    }
-}
-*/
-
-import java.util.Scanner;
-public class aaa{
-    public static void main(String[] args){
-        System.out.println("Please input:");
-        Scanner a = new Scanner(System.in);
-        int b = a.nextInt();
-        int c = a.nextInt();
-        int d = a.nextInt();
-        double e = ((b+c+d)/3.0);
-        System.out.println("Average: "+e);
     }
 }
